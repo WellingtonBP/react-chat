d0c5cec5bcd5b5dc0b8cbef78954f1879de0d268
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2021-07-09 00:03:21 -0300

    [Server ]Add mongoose models

---
+++ server/src/models/Chat.ts
@@ -0,0 +1,31 @@
+import { Schema, model, Types, Document, ObjectId } from 'mongoose'
+
+interface IChat extends Document {
+  messages: Array<{
+    content: string
+    sender: ObjectId
+    senderAt: number
+  }>
+}
+
+const chatSchema = new Schema({
+  messages: [
+    {
+      content: {
+        type: String,
+        required: true
+      },
+      sender: {
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      senderAt: {
+        type: Number,
+        required: true
+      }
+    }
+  ]
+})
+
+export default model<IChat>('Chat', chatSchema)
---
+++ server/src/models/User.ts
@@ -0,0 +1,80 @@
+import { Schema, model, Types, Document, ObjectId } from 'mongoose'
+
+interface IUser extends Document {
+  name: string
+  email: string
+  password: string
+  avatar?: string
+  isOnline: boolean
+  requestsReceived: Array<ObjectId>
+  requestsSent: Array<ObjectId>
+  friends: Array<{
+    friendId: ObjectId
+    unreadMessages: number
+    isRemoved: boolean
+    cleanedAt?: number
+    chatId: ObjectId
+  }>
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: String,
+  isOnline: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  requestsReceived: [
+    {
+      type: Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  ],
+  requestsSent: [
+    {
+      type: Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  ],
+  friends: [
+    {
+      friendId: {
+        type: String,
+        ref: 'User',
+        required: true
+      },
+      unreadMessages: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      isRemoved: {
+        type: Boolean,
+        required: true,
+        default: false
+      },
+      cleanedAt: Number,
+      chatId: {
+        type: Types.ObjectId,
+        ref: 'Chat',
+        required: true
+      }
+    }
+  ]
+})
+
+export default model<IUser>('User', userSchema)
